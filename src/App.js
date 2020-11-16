fdb0b22ba8ee74adfef45c4e9727d8b7f7312569
zhangjunjie <zhangjunjie0730@163.com>
2020-11-16 10:32:42 +0800

    layout-zjj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './assets/css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div class="flex">
+      <h1>2048</h1>
+      <div class="setting">
+        <a href="#" id="newGameBtn">
+          重新开始
         </a>
-      </header>
+        <select class="difficulty-mode">
+          <option value="easy">游戏难度</option>
+          <option value="easy">简单</option>
+          <option value="middle">普通</option>
+          <option value="hard">困难</option>
+        </select>
+        <select class="game-mode">
+          <option value="classic">游戏模式</option>
+          <option value="classic">经典模式</option>
+          <option value="combat">对战模式</option>
+        </select>
+      </div>
+      <p>
+        score: <span id="score">0</span>
+      </p>
+      <div class="container">
+        <div class="grid-cell" id="grid-cell-0-0"></div>
+        <div class="grid-cell" id="grid-cell-0-1"></div>
+        <div class="grid-cell" id="grid-cell-0-2"></div>
+        <div class="grid-cell" id="grid-cell-0-3"></div>
+        <div class="grid-cell" id="grid-cell-1-0"></div>
+        <div class="grid-cell" id="grid-cell-1-1"></div>
+        <div class="grid-cell" id="grid-cell-1-2"></div>
+        <div class="grid-cell" id="grid-cell-1-3"></div>
+        <div class="grid-cell" id="grid-cell-2-0"></div>
+        <div class="grid-cell" id="grid-cell-2-1"></div>
+        <div class="grid-cell" id="grid-cell-2-2"></div>
+        <div class="grid-cell" id="grid-cell-2-3"></div>
+        <div class="grid-cell" id="grid-cell-3-0"></div>
+        <div class="grid-cell" id="grid-cell-3-1"></div>
+        <div class="grid-cell" id="grid-cell-3-2"></div>
+        <div class="grid-cell" id="grid-cell-3-3"></div>
+      </div>
     </div>
   );
 }
