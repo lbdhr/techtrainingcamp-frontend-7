b752d7dac8b32d9c6d07aa2b1981fca321a58dd0
zhangjunjie <zhangjunjie0730@163.com>
2020-12-02 11:03:06 +0800

    gameover page

--- src/components/GameOver/index.js
+++ src/components/GameOver/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { reset } from '../../reducers/gameReducer';
 import Modal from '../Modal';
 import './gameOver.css';
 
-export default function GameOver(props) {
+function GameOver(props) {
   return (
     <div>
-      <Modal display={true}>
+      <Modal display={props.gameOver}>
         <div>
-          <div className="gameover-container ">xianshi gameover</div>
+          <div className="gameover-container ">
+            <h3>Game Over</h3>
+            <h4>Score: {props.score}</h4>
+            <button onClick={props.onReset}>重新开始</button>
+          </div>
         </div>
       </Modal>
     </div>
   );
 }
+const mapStateToProps = state => ({
+  score: state.present.gameReducer.score,
+  gameOver: state.present.gameReducer.gameOver,
+});
+const mapDispatchToProps = {
+  onReset: reset,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameOver);
