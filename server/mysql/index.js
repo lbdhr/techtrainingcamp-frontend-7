bcc2cefd2e5d08747de67a846228a8c9cc215d6d
BaiLi <bai-li@foxmail.com>
2020-12-06 18:04:31 +0800

    Update index.js

--- server/mysql/index.js
+++ server/mysql/index.js
@@ -16,6 +16,21 @@ client.connect(function(err) {
     console.log('connected as id ' + client.threadId);
 });
 
+client.on('error', function(err){
+   console.log('db error', err);
+   // 如果是自动断开，自动重连
+    console.info('如果是连接断开，则自动重连');
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+        client.connect(function(err) {
+            if (err) {
+                console.error('error connecting: ' + err.stack);
+                return;
+            }
+            console.log('connected as id ' + client.threadId);
+        });
+    } else {throw err;}
+});
+
 function sqlFn(sql,arr,callback){
     client.query(sql,arr,function(error,result){
         if(error){
