93531cbfef1ea14ae328e287cfad8d35f58f033d
lbdhr <530155947@qq.com>
2020-12-31 15:42:19 +0800

    Update index.js

--- server/mysql/index.js
+++ server/mysql/index.js
@@ -5,31 +5,34 @@ var client = mysql.createConnection({
     user:"root", // 默认的
     password:"",
     database:"iwenuser"
-})
-
-client.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-
-    console.log('connected as id ' + client.threadId);
 });
 
-client.on('error', function(err){
-   console.log('db error', err);
-   // 如果是自动断开，自动重连
-    console.info('如果是连接断开，则自动重连');
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-        client.connect(function(err) {
-            if (err) {
-                console.error('error connecting: ' + err.stack);
-                return;
-            }
-            console.log('connected as id ' + client.threadId);
-        });
-    } else {throw err;}
-});
+function handleError() {
+
+    client.connect(function(err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return;
+        }
+
+        console.log('connected as id ' + client.threadId);
+    });
+
+    client.on('error', function(err){
+        console.log('db error', err);
+        // 如果是自动断开，自动重连
+        console.info('如果是连接断开，则自动重连');
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+            client.connect(function(err) {
+                if (err) {
+                    console.error('error connecting: ' + err.stack);
+                    return;
+                }
+                console.log('connected as id ' + client.threadId);
+            });
+        } else {throw err;}
+    });
+}
 
 function sqlFn(sql,arr,callback){
     client.query(sql,arr,function(error,result){
@@ -40,5 +43,7 @@ function sqlFn(sql,arr,callback){
         callback(result)
     })
 }
+
+handleError();
 
 module.exports = sqlFn
